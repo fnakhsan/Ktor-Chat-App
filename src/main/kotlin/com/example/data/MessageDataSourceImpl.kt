d4e20ed37c1fd3762c21a46f8d9ce5820525a5f1
fnakhsan <fn.akhsan@gmail.com>
2022-08-24 20:37:11 +0700

    Initial Commit

---
+++ src/main/kotlin/com/example/data/MessageDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.example.data
+
+import com.example.data.model.Message
+import org.litote.kmongo.coroutine.CoroutineDatabase
+
+class MessageDataSourceImpl(
+    db: CoroutineDatabase
+) : MessageDataSource {
+
+    private val messages = db.getCollection<Message>()
+
+    override suspend fun getAllMessages(): List<Message> {
+        return messages.find().descendingSort(Message::timestamp).toList()
+    }
+
+    override suspend fun insertMessage(message: Message) {
+        messages.insertOne(message)
+    }
+
+
+}

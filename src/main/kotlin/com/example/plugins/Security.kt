e9a11421d65567b67ec635640e5deb2a2d19a64b
fnakhsan <fn.akhsan@gmail.com>
2022-08-24 22:16:04 +0700

    Add Session and RoomController

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -1,24 +1,21 @@
 package com.example.plugins
 
+import com.example.session.ChatSession
 import io.ktor.server.sessions.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.ApplicationCallPipeline.ApplicationPhase.Plugins
+import io.ktor.util.*
+
 
 fun Application.configureSecurity() {
-    data class MySession(val count: Int = 0)
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
+        cookie<ChatSession>("SESSION")
     }
 
-    routing {
-        get("/session/increment") {
-                val session = call.sessions.get<MySession>() ?: MySession()
-                call.sessions.set(session.copy(count = session.count + 1))
-                call.respondText("Counter is ${session.count}. Refresh to increment.")
-            }
+    intercept(Plugins){
+        if (call.sessions.get<ChatSession>() == null){
+            val username =  call.parameters["username"] ?: "Guest"
+            call.sessions.set(ChatSession(username, generateNonce()))
+        }
     }
 }
---
+++ src/main/kotlin/com/example/session/ChatSession.kt
@@ -0,0 +1,6 @@
+package com.example.session
+
+data class ChatSession(
+    val username: String,
+    val sessionId: String
+)
